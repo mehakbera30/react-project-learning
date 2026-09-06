91c842b8860844e707b07091935d00a20149914f
mehak bera <beramehak0@gmail.com>
2026-09-06 10:43:19 +0530

    advanced react

--- Advanced-useState/src/App.jsx
+++ Advanced-useState/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ use, useState } from 'react'
 
 const App = () => {
   const [name , setname] = useState({user:'mehak', age:29});
@@ -9,10 +9,21 @@ const App = () => {
      newName.age = 27;
      setname(newName);
   }
+  
+  const [num, setvalue] = useState([23,45,54]);
+  const button_push = () => {
+    const newnum = [...num];
+    newnum.push(99);
+    setvalue(newnum);
+
+  }
   return (
     <div>
-      <h1>the name of the student is {name.user} and the age of the person is {name.age}</h1>
-      <button onClick={buttonclicked}>click me</button>
+      {/* <h1>the name of the student is {name.user} and the age of the person is {name.age}</h1>
+      <button onClick={buttonclicked}>click me</button> */}
+
+      <h1>{num}</h1>
+      <button onClick={button_push}>click me</button>
     </div>
   )
 }
